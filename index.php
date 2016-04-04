a24a2be2b8fbc8792c4fd710bf36c80a0a7aaa0c
daulat9107 <djwadh910@gmail.com>
2016-04-05 00:40:46 +0530

    Updated composer file and deleted init file

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
+require 'vendor/autoload.php';
 use Daulat\System\App;
 use Daulat\System\Config\Config;
-require 'app/init.php';
 $app=new App();
 $app->container->singleton('config',function(){
-return new Config('app/Config/production.php');
+return new Config('app/Config/development.php');
 });
-$app->config->get('db.driver');
+echo $app->config->get('db.driver');
 
 
 
